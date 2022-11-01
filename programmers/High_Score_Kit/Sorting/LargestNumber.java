c880741b59391fee94ca4d2962696d22bd171eb7
joonghyun <80939285+joong2043@users.noreply.github.com>
2022-11-01 17:31:28 +0900

    가장 큰 수 해결

---
+++ programmers/High_Score_Kit/Sorting/LargestNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        String[] str = new String[numbers.length];
+        for(int i =0; i<numbers.length;i++){
+            str[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(str,new Comparator<String>(){
+            public int compare(String o1, String o2){
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        for(String number : str){
+            answer += number;
+        }
+
+        if(str[0].equals("0")){
+            answer = "0";
+        }
+
+        return answer;
+    }
+}
